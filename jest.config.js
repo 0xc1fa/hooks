c1f7c25707eadb2e540afe91358a4b628133a559
Yat Fu Chan <45863731+chanyatfu@users.noreply.github.com>
2024-04-11 21:48:25 +0800

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "/^@\/(.*)$/": "<rootDir>/$1"
+  },
+};
