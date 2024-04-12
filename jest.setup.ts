50bcbddc286d67fa44e730b9d776fd25f43f2556
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-13 04:42:03 +0800

    test: add createObjectURL mock to jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,3 +4,13 @@ jest.spyOn(global, "requestAnimationFrame").mockImplementation((fn) => {
 jest.spyOn(global, "cancelAnimationFrame").mockImplementation((id) => {
   clearTimeout(id);
 });
+
+import { v4 as uuidv4 } from "uuid";
+global.URL.createObjectURL = jest.fn((obj) => {
+  if (!(obj instanceof Blob || obj instanceof MediaSource)) {
+    throw new Error("Invalid object type");
+  }
+  return obj instanceof Blob
+    ? `blob:domain.test/${uuidv4()}`
+    : `media:domain.test/${uuidv4()}`;
+});
