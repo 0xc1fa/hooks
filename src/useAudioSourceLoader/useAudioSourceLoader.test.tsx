0de10cce4aa518ce99ea423ccf8180e8a5852425
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-13 04:43:20 +0800

    test: refactor and cleanup tests of useAudioSourceLoader

--- src/useAudioSourceLoader/useAudioSourceLoader.test.tsx
+++ src/useAudioSourceLoader/useAudioSourceLoader.test.tsx
@@ -1,79 +1,47 @@
 import { useAudioSourceLoader } from "./useAudioSourceLoader";
-import { useObjectURL, type ObjectWithURL } from "../useObjectURL";
 import { renderHook } from "@testing-library/react";
-import { RefObject } from "react";
+import { useRef } from "react";
+
+function createMockAudioElement(paused: boolean) {
+  return {
+    paused: paused,
+    load: jest.fn(),
+    play: jest.fn(() => Promise.resolve()),
+    pause: jest.fn(),
+    src: "",
+  } as unknown as HTMLAudioElement;
+}
 
 describe("useAudioSourceLoader", () => {
-  beforeEach(() => {
-    global.URL.createObjectURL = jest.fn(() => "mockAudioUrl");
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-  })
-
   it("does nothing if audio ref is null", () => {
-    const audioRef = {
-      current: null,
-    };
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const dummyBlob = new Blob(["dummy content"], { type: "audio/mp3" });
+    const dummyUrl = global.URL.createObjectURL(dummyBlob);
 
-    const blobWithUrl: ObjectWithURL<Blob> = {
-      obj: new Blob(["dummy content"], { type: "audio/mp3" }),
-      url: "mockAudioUrl",
-    };
-
-    renderHook(() => useAudioSourceLoader(audioRef, blobWithUrl.url));
+    renderHook(() => useAudioSourceLoader(audioRef, dummyUrl));
     expect(audioRef.current).toBeNull();
-    jest.resetAllMocks();
   });
 
   it("sets audio source and loads new audio when provided", () => {
-   const audioRef = {
-      current: {
-        paused: true,
-        load: jest.fn(),
-        play: jest.fn(() => Promise.resolve()),
-        pause: jest.fn(),
-        src: "",
-      },
-    } as unknown as RefObject<HTMLAudioElement>;
-
+    const audioRef = useRef(createMockAudioElement(true));
     const dummyBlob = new Blob(["dummy content"], { type: "audio/mp3" });
-    const { result } = renderHook(() => useObjectURL(dummyBlob));
+    const dummyUrl = global.URL.createObjectURL(dummyBlob);
 
-    const blobWithUrl: ObjectWithURL<Blob> | null = result.current[0];
-    if (!blobWithUrl) {
-      throw new Error("blobWithUrl is null");
-    }
-    renderHook(() => useAudioSourceLoader(audioRef, blobWithUrl.url));
-    expect(audioRef.current?.src).toBe(blobWithUrl.url);
+    renderHook(() => useAudioSourceLoader(audioRef, dummyUrl));
+    expect(audioRef.current?.src).toBe(dummyUrl);
     expect(audioRef.current?.load).toHaveBeenCalledTimes(1);
     expect(audioRef.current?.play).not.toHaveBeenCalled();
-    jest.resetAllMocks();
   });
 
   it("pauses, sets source, and reloads if audio is playing", () => {
-    const audioRef = {
-      current: {
-        paused: false,
-        load: jest.fn(),
-        play: jest.fn(() => Promise.resolve()),
-        pause: jest.fn(),
-        src: "",
-      },
-    } as unknown as RefObject<HTMLAudioElement>;
+    const audioRef = useRef(createMockAudioElement(false));
     const dummyBlob = new Blob(["dummy content"], { type: "audio/mp3" });
-    const { result } = renderHook(() => useObjectURL(dummyBlob));
+    const dummyURL = global.URL.createObjectURL(dummyBlob);
 
-    const blobWithUrl: ObjectWithURL<Blob> | null = result.current[0];
-    if (!blobWithUrl) {
-      throw new Error("blobWithUrl is null");
-    }
-    renderHook(() => useAudioSourceLoader(audioRef, blobWithUrl.url));
-    expect(audioRef.current?.src).toBe(blobWithUrl.url);
+    renderHook(() => useAudioSourceLoader(audioRef, dummyURL));
+    expect(audioRef.current?.src).toBe(dummyURL);
     expect(audioRef.current?.pause).toHaveBeenCalled();
     expect(audioRef.current?.load).toHaveBeenCalledTimes(1);
     expect(audioRef.current?.play).toHaveBeenCalled();
-    jest.resetAllMocks();
   });
 });
