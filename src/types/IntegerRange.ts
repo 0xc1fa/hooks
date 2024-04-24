2d0b3df38e8bd42057e03e5d5f75337832334fa3
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-24 20:36:11 +0800

    feat: add IntegerRange type

---
+++ src/types/IntegerRange.ts
@@ -0,0 +1,13 @@
+export { type IntegerRange };
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc["length"] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc["length"]]>;
+
+type IntegerRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
