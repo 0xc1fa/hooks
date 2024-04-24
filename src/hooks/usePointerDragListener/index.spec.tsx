896cc05a058d7009d65dd3127dbcf4144561d8b0
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-24 21:16:57 +0800

    feat: simplify usePointerDragListener

--- src/hooks/usePointerDragListener/index.spec.tsx
+++ src/hooks/usePointerDragListener/index.spec.tsx
@@ -1,43 +1,50 @@
-// import { fireEvent, waitFor } from "@testing-library/react";
-// import { renderHook, act } from "@testing-library/react";
-// import { PointerActiveInfo, usePointerDraggingListener } from ".";
+import { fireEvent, waitFor } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
+import { usePointerDragListener } from ".";
 
-// describe("useActivePointerState", () => {
-//   let ref: { current: HTMLDivElement };
-//   let result: { current: PointerActiveInfo };
-//   let unmount: () => void;
+describe("useActivePointerState", () => {
+  let ref: { current: HTMLDivElement };
+  let callback: jest.Mock;
+  let unmount: () => void;
 
-//   beforeEach(() => {
-//     ref = { current: document.createElement("div") };
-//     ({ result, unmount } = renderHook(() => useActivePointerState(ref)));
-//   });
+  beforeEach(() => {
+    ref = { current: document.createElement("div") };
+    callback = jest.fn();
+    ({ unmount } = renderHook(() => usePointerDragListener(ref, callback)));
+  });
 
-//   test("initializes as inactive", () => {
-//     expect(result.current.isPointerDown).toBe(false);
-//   });
+  test("initializes as inactive", () => {
+    expect(callback).not.toHaveBeenCalled();
+  });
 
-//   test("activates on pointerdown", () => {
-//     act(() => fireEvent.pointerDown(ref.current));
-//     expect(result.current.isPointerDown).toBe(true);
-//   });
+  test("activates on pointerdown", () => {
+    act(() => fireEvent.pointerDown(ref.current));
+    expect(callback).not.toHaveBeenCalled();
+  });
 
-//   test("deactivates on pointerup", () => {
-//     act(() => fireEvent.pointerDown(ref.current));
-//     act(() => fireEvent.pointerUp(document));
-//     expect(result.current.isPointerDown).toBe(false);
-//   });
+  test("calls callback on pointermove", () => {
+    act(() => fireEvent.pointerDown(ref.current));
+    act(() => fireEvent.pointerMove(ref.current));
+    expect(callback).toHaveBeenCalled();
+  });
 
-//   test("deactivates on pointercancel", () => {
-//     act(() => fireEvent.pointerDown(ref.current));
-//     act(() => fireEvent.pointerCancel(document));
-//     expect(result.current.isPointerDown).toBe(false);
-//   });
+  test("deactivates on pointerup", () => {
+    act(() => fireEvent.pointerDown(ref.current));
+    act(() => fireEvent.pointerUp(document));
+    act(() => fireEvent.pointerMove(document));
+    expect(callback).not.toHaveBeenCalled();
+  });
 
-//   test("deactivates upon unmounting", () => {
-//     act(() => fireEvent.pointerDown(ref.current));
-//     unmount();
-//     waitFor(() => !result.current.isPointerDown).then(() =>
-//       expect(result.current.isPointerDown).toBe(false)
-//     );
-//   });
-// });
+  test("deactivates on pointercancel", () => {
+    act(() => fireEvent.pointerDown(ref.current));
+    act(() => fireEvent.pointerCancel(document));
+    act(() => fireEvent.pointerMove(document));
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  test("deactivates upon unmounting", () => {
+    act(() => fireEvent.pointerDown(ref.current));
+    unmount();
+    expect(callback).not.toHaveBeenCalled();
+  });
+});
--- src/hooks/usePointerDragListener/index.tsx
+++ src/hooks/usePointerDragListener/index.tsx
@@ -1,97 +1,40 @@
-export {
-  usePointerDragListener,
-  type PointerActiveInfo,
-  type PointerClientPosition,
-};
+export { usePointerDragListener, type PointerDragListenerOptions };
 
-import { useState } from "react";
+import { useRef } from "react";
 import { useEventListener } from "@/hooks/useEventListener";
-import { useUnmount } from "../useUnmount";
+import { useUnmount } from "@/hooks/useUnmount";
 
-type PointerActivateState<Buffer> = {
-  isPointerDown: true;
-  initialPosition: PointerClientPosition;
-  buffer: Buffer;
-};
-
-type PointerInactiveState = {
-  isPointerDown: false;
-  initialPosition: null;
-  buffer: null;
-};
-
-type PointerActiveInfo<Buffer> =
-  | PointerActivateState<Buffer>
-  | PointerInactiveState;
-
-type PointerClientPosition = {
-  x: number;
-  y: number;
+type PointerDragListenerOptions = {
+  capturePointer?: boolean;
+  lockPointer?: boolean;
 };
 
 function usePointerDragListener(
   ref: React.RefObject<HTMLElement>,
-  callback: (event: PointerEvent, info: PointerActivateState<undefined>) => void
-): PointerActiveInfo<undefined>;
+  callback: (event: PointerEvent) => void,
+  options?: PointerDragListenerOptions
+): void {
+  const isPointerDown = useRef(false);
 
-function usePointerDragListener<Buffer>(
-  ref: React.RefObject<HTMLElement>,
-  callback: (event: PointerEvent, info: PointerActivateState<Buffer>) => void,
-  bufferCallback: (event: PointerEvent) => Buffer
-): PointerActiveInfo<Buffer>;
-
-function usePointerDragListener<Buffer = undefined>(
-  ref: React.RefObject<HTMLElement>,
-  callback: (event: PointerEvent, info: PointerActivateState<Buffer>) => void,
-  bufferCallback?: (event: PointerEvent) => Buffer
-): PointerActiveInfo<Buffer> {
-  const [pointerActiveInfo, setPointerActiveInfo] = useState<
-    PointerActiveInfo<Buffer>
-  >(createInactiveInfo());
-
-  useEventListener(ref, "pointerdown", (event) => {
-    ref.current?.setPointerCapture(event.pointerId);
-    const bufferValue = bufferCallback
-      ? bufferCallback(event)
-      : (undefined as Buffer);
-    setPointerActiveInfo(
-      createActiveInfo<Buffer>(
-        { x: event.clientX, y: event.clientY },
-        bufferValue
-      )
-    );
-  });
+  useEventListener(ref, "pointerdown", setup);
 
   useEventListener(ref, "pointermove", (event) => {
-    if (pointerActiveInfo.isPointerDown) {
-      callback(event, pointerActiveInfo);
-    }
+    if (isPointerDown.current) callback(event);
   });
 
-  useEventListener(document, ["pointercancel", "pointerup"], () => {
-    setPointerActiveInfo(createInactiveInfo());
-  });
+  useEventListener(document, ["pointercancel", "pointerup"], clearup);
 
-  useUnmount(() => setPointerActiveInfo(createInactiveInfo()));
+  useUnmount(clearup);
 
-  return pointerActiveInfo;
-}
+  function setup(event: PointerEvent) {
+    isPointerDown.current = true;
+    if (options?.lockPointer) ref.current?.requestPointerLock();
+    if (options?.capturePointer)
+      ref.current?.setPointerCapture(event.pointerId);
+  }
 
-function createActiveInfo<Buffer>(
-  initialPosition: PointerClientPosition,
-  bufferValue: Buffer
-): PointerActivateState<Buffer> {
-  return {
-    isPointerDown: true,
-    initialPosition: initialPosition,
-    buffer: bufferValue,
-  };
-}
-
-function createInactiveInfo(): PointerInactiveState {
-  return {
-    isPointerDown: false,
-    initialPosition: null,
-    buffer: null,
-  };
+  function clearup() {
+    options?.lockPointer && document.exitPointerLock();
+    isPointerDown.current = false;
+  }
 }
